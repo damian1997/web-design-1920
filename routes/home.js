10fb5473e9870626938da84322e1d675444bd518
damian1997 <damianveltkamp@Damians-MacBook-Pro.local>
2020-04-22 14:33:12 +0200

    Added the bundle to home route

--- routes/home.js
+++ routes/home.js
@@ -1,4 +1,4 @@
-export default async function(req,res,COMPONENTPATH) {
+export default async function(req,res,COMPONENTPATH,BUNDLE) {
   res.render(`${COMPONENTPATH}/base/views/home`, {
     bundledCSS: BUNDLE['main.css'],
     bundledJS: BUNDLE['main.js']
